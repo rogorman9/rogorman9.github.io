b4e2e56829c6cf2e089643a3b37340ab0ecd8915
rogorman9 <ryanogorman@WTA-MBP419.local>
2018-11-14 14:11:55 -0500

    Static updates?

---
+++ precache-manifest.426d98ae80620c36211c158ed53ad9c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92ac4e60cccb4c7fc7c0",
+    "url": "/static/js/main.92ac4e60.chunk.js"
+  },
+  {
+    "revision": "67c5e54987b6d8f54bc2",
+    "url": "/static/js/1.67c5e549.chunk.js"
+  },
+  {
+    "revision": "92ac4e60cccb4c7fc7c0",
+    "url": "/static/css/main.c1a276c0.chunk.css"
+  },
+  {
+    "revision": "61bb062e9653abe710204014747d78d7",
+    "url": "/index.html"
+  }
+];
