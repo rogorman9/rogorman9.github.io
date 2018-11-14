d62e476fa806ed6f38a82a962100752a6ce4cea0
rogorman9 <ryanogorman@WTA-MBP419.local>
2018-11-14 13:51:13 -0500

    Updates

---
+++ precache-manifest.6b3d46ad75b1298364be52e2d68d50f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f484ed0d0312e8c6b3f9",
+    "url": "/static/js/main.f484ed0d.chunk.js"
+  },
+  {
+    "revision": "67c5e54987b6d8f54bc2",
+    "url": "/static/js/1.67c5e549.chunk.js"
+  },
+  {
+    "revision": "f484ed0d0312e8c6b3f9",
+    "url": "/static/css/main.c1a276c0.chunk.css"
+  },
+  {
+    "revision": "4ed92bc7035dc9792b39d8b5a9c44c72",
+    "url": "/index.html"
+  }
+];
